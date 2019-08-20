443f969a9401c047f2de33ae9181426517a4146d
luiztauffer <luiz_tauffer@hotmail.com>
2019-08-20 10:34:13 +0200

    major reorganization - added functions to be imported - added example script to run - setup.py and make_env.yml - tutorials folder

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+# Get the long description from the README file
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(name = 'axel_lab_to_nwb',
+      version = '0.0.1',
+      description = 'NWB conversion scripts and tutorials.',
+      long_description = long_description,
+      long_description_content_type = 'text/markdown',
+      author = 'Luiz Tauffer and Ben Dichter',
+      email = 'ben.dichter@gmail.com',
+      packages=find_packages(),
+      include_package_data = True,
+      install_requires = ['matplotlib', 'cycler', 'scipy', 'numpy', 'jupyter',
+                          'h5py', 'pynwb'],
+      )
---
+++ tutorials/example_script.py
@@ -0,0 +1,24 @@
+#
+# Example: how to use the conversion script
+# ------------------------------------------------------------------------------
+
+from datetime import datetime
+from dateutil.tz import tzlocal
+from axel_lab_to_nwb.npz_to_nwb import * 
+
+fpath = '/Users/bendichter/Desktop/Axel Lab/data/2019_07_01_fly2'
+f1 = '2019_07_01_Nsyb_NLS6s_walk_fly2.npz'
+f2 = '2019_07_01_Nsyb_NLS6s_walk_fly2_A.npz'
+f3 = '2019_07_01_Nsyb_NLS6s_walk_fly2_ref_im.npz'
+fnpz = [f1, f2, f3]
+fnwb = 'fly2.nwb'
+info = {'session_description':'my CaIm recording',
+        'identifier':'EXAMPLE_ID',
+        'session_start_time':datetime.now(tzlocal()),
+        'experimenter':'Evan Schaffer',
+        'lab':'Axel lab',
+        'institution':'Columbia University',
+        'experiment_description':'EXPERIMENT_DESCRIPTION',
+        'session_id':'IDX'}
+
+npz_to_nwb(fpath=fpath, fnpz=fnpz, fnwb=fnwb, info=info, plot_rois=False)
