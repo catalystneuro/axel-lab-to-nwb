b3627d734f6a90453a02b0fb20a6ce1ac93c991d
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2019-08-27 15:28:00 -0400

    * clean up imports * fix RoiTableRegion range

--- axel_lab_to_nwb/npz_to_nwb.py
+++ axel_lab_to_nwb/npz_to_nwb.py
@@ -10,9 +10,7 @@ from pynwb.base import TimeSeries
 from pynwb.behavior import SpatialSeries, Position
 from ndx_grayscalevolume import GrayscaleVolume
 
-import scipy.io
 import numpy as np
-import h5py
 import os
 import matplotlib.pyplot as plt
 
@@ -34,7 +32,7 @@ def npz_to_nwb(fpath, fnpz, fnwb, info, plot_rois=False):
     plot_rois : boolean
         If True plots 3D ROIs, if False skips it.
     """
-    #Load data from .npz files
+    # Load data from .npz files
     files_path = fpath
 
     fname1 = fnpz[0]
@@ -49,7 +47,7 @@ def npz_to_nwb(fpath, fnpz, fnwb, info, plot_rois=False):
     fpath3 = os.path.join(files_path, fname3)
     file3 = np.load(fpath3)
 
-    #Initialize a NWB object
+    # Initialize a NWB object
     nwb = NWBFile(
         session_description=info['session_description'],
         identifier=info['identifier'],
@@ -61,12 +59,12 @@ def npz_to_nwb(fpath, fnpz, fnwb, info, plot_rois=False):
         session_id=info['session_id'],
     )
 
-    #Create and add device
+    # Create and add device
     device = Device('Device')
     nwb.add_device(device)
 
     # Create an Imaging Plane
-    fs =1/(file1['time'][0][1]-file1['time'][0][0])
+    fs = 1. / (file1['time'][0][1]-file1['time'][0][0])
     tt = file1['time'].ravel()
     optical_channel = OpticalChannel(
         name='OpticalChannel',
@@ -98,16 +96,16 @@ def npz_to_nwb(fpath, fnpz, fnwb, info, plot_rois=False):
     )
     nwb.add_processing_module(ophys_module)
 
-    #Create Image Segmentation compartment
+    # Create Image Segmentation compartment
     img_seg = ImageSegmentation()
     ophys_module.add(img_seg)
 
-    #Create plane segmentation and add ROIs
+    # Create plane segmentation and add ROIs
     ps = img_seg.create_plane_segmentation(
         description='plane segmentation',
         imaging_plane=imaging_plane,
     )
-    #Call function
+    # Call function
     indices = file2['indices']
     indptr = file2['indptr']
     dims = np.squeeze(file1['dims'])
@@ -116,30 +114,31 @@ def npz_to_nwb(fpath, fnpz, fnwb, info, plot_rois=False):
         voxel_mask = make_voxel_mask(indices[start:stop], dims)
         ps.add_roi(voxel_mask=voxel_mask)
 
-    #Visualize 3D voxel masks
+    # Visualize 3D voxel masks
     if plot_rois:
         from mpl_toolkits.mplot3d import Axes3D
         from itertools import cycle
 
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-        for select, c in zip(range(len(indptr)-1),cycle(['r','g','k','b','m','w','y','brown'])):
+        for select, c in zip(range(len(indptr)-1),
+                             cycle(['r', 'g', 'k', 'b', 'm', 'w', 'y', 'brown'])):
             x, y, z, _ = np.array(ps['voxel_mask'][select]).T
             ax.scatter(x, y, z, c=c, marker='.')
 
-    #DFF measures
+    # DFF measures
     dff = DfOverF(name='DfOverF')
     ophys_module.add(dff)
 
-    #create ROI regions
+    # create ROI regions
     roi_region = ps.create_roi_table_region(
         description='RoiTableRegion',
-        region=[0]
+        region=list(range(len(indptr)-1))
     )
 
-    #create ROI response series
+    # create ROI response series
     dff_data = file1['dFF']
-    dFF_series = dff.create_roi_response_series(
+    dff.create_roi_response_series(
         name='RoiResponseSeries',
         data=dff_data.T,
         unit='NA',
@@ -147,12 +146,12 @@ def npz_to_nwb(fpath, fnpz, fnwb, info, plot_rois=False):
         timestamps=tt
     )
 
-    #Creates GrayscaleVolume containers and add a reference image
+    # Creates GrayscaleVolume containers and add a reference image
     grayscale_volume = GrayscaleVolume(name='GrayscaleVolume',
                                        data=file3['im'])
     ophys_module.add(grayscale_volume)
 
-    #Trial times
+    # Trial times
     tt = file1['time'].ravel()
     trialFlag = file1['trialFlag'].ravel()
     trial_inds = np.hstack((0, np.where(np.diff(trialFlag))[0], trialFlag.shape[0]-1))
@@ -161,7 +160,7 @@ def npz_to_nwb(fpath, fnpz, fnwb, info, plot_rois=False):
     for start, stop in zip(trial_times, trial_times[1:]):
         nwb.add_trial(start_time=start, stop_time=stop)
 
-    #Behavior data - ball motion
+    # Behavior data - ball motion
     behavior_mod = nwb.create_processing_module(
         name='behavior',
         description='holds processed behavior data',
@@ -171,12 +170,12 @@ def npz_to_nwb(fpath, fnpz, fnwb, info, plot_rois=False):
                                 timestamps=tt,
                                 unit='unknown'))
 
-    #Re-arranges spatial data of body-points positions tracking
+    # Re-arranges spatial data of body-points positions tracking
     pos = file1['dlc']
     nPoints = 8
-    pos_reshaped = pos.reshape((-1,nPoints,3))  #dims=(nSamples,nPoints,3)
+    pos_reshaped = pos.reshape((-1, nPoints, 3))  # dims=(nSamples,nPoints,3)
 
-    #Creates a Position object and add one SpatialSeries for each body-point position
+    # Creates a Position object and add one SpatialSeries for each body-point position
     position = Position()
     for i in range(nPoints):
         position.create_spatial_series(name='SpatialSeries_'+str(i),
@@ -185,12 +184,11 @@ def npz_to_nwb(fpath, fnpz, fnwb, info, plot_rois=False):
                                        reference_frame='Description defining what the zero-position is.')
     behavior_mod.add(position)
 
-    #Saves to NWB file
+    # Saves to NWB file
     fpath_nwb = os.path.join(fpath, fnwb)
     with NWBHDF5IO(fpath_nwb, mode='w') as io:
         io.write(nwb)
     print('NWB file saved with size: ', os.stat(fpath_nwb).st_size/1e6, ' mb')
-
 
 
 def make_voxel_mask(indices, dims):
@@ -201,8 +199,8 @@ def make_voxel_mask(indices, dims):
     voxel_mask = []
     for ind in indices:
         zp = np.floor(ind/(dims[0]*dims[1])).astype('int')
-        rest = ind%(dims[0]*dims[1])
+        rest = ind % (dims[0]*dims[1])
         yp = np.floor(rest/dims[0]).astype('int')
-        xp = rest%dims[0]
-        voxel_mask.append((xp,yp,zp,1))
+        xp = rest % dims[0]
+        voxel_mask.append((xp, yp, zp, 1))
     return voxel_mask
