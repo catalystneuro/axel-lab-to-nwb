c0ca6640a6e270334fcc81f0cc4b2b5cd5023b52
luiztauffer <luiz_tauffer@hotmail.com>
2019-09-18 13:53:21 +0300

    Update nwb_gui.py

--- axel_lab_to_nwb/nwb_gui.py
+++ axel_lab_to_nwb/nwb_gui.py
@@ -2,9 +2,9 @@
 # authors: Luiz Tauffer and Ben Dichter
 # written for Axel Lab
 # ------------------------------------------------------------------------------
-from nwbn_conversion_tools.gui.main_gui import main
+from nwbn_conversion_tools.gui.nwbn_conversion_gui import nwbn_conversion_gui
 
 metafile = 'metafile.yml'
 conversion_module = 'conversion_module.py'
 
-main(metafile=metafile, conversion_module=conversion_module)
+nwbn_conversion_gui(metafile=metafile, conversion_module=conversion_module)
